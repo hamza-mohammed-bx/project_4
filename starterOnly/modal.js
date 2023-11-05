ff76e71f6def9fee1cc7574d847b035e17d09262
Mohammed Habib Hamza <microrepair33@gmail.com>
2023-11-05 20:51:39 +0100

    create a function to stop modal onclick cross

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,21 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const modalIcon = document.querySelector(".close");
+
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalIcon.addEventListener("click", stopModal);
+
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+}
+// stop  modal form
+function stopModal() {
+  modalbg.style.display = "none";
 }
 
 
