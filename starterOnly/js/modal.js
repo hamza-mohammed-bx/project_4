35f6192ae9a9b0597a76c16ace8c5f1f408816b7
Mohammed Habib Hamza <microrepair33@gmail.com>
2023-11-13 19:15:10 +0100

    create a function for launching a responsive  nav menu

--- starterOnly/js/modal.js
+++ starterOnly/js/modal.js
@@ -1,32 +1,59 @@
-function editNav() {
-  let changNav = document.getElementById("myTopnav");
-  if (changNav.className === "topnav") {
-    changNav.className += " responsive";
-  } else {
-    changNav.className = "topnav";
-    
-  }
-}
 
 // DOM Elements
+
+// Modal declaration elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
-const modalIcon = document.querySelector(".close");
+const modalIcon = document.querySelector(".close-modal");
 
+// Nav declaration elements
+let changNav = document.querySelector("#myTopnav");
+const navBar = document.querySelector(".main-navbar");
+const navBtnClos = document.querySelectorAll(".close-nav");
+const navIcon = document.querySelectorAll(".icon")
+
+// *************** modal event ***************//
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-modalIcon.addEventListener("click", stopModal);
 
+// Stop modal event
+modalIcon.addEventListener("click", stopModal);
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
-// stop  modal form
+
+// close  modal form
 function stopModal() {
   modalbg.style.display = "none";
 }
 
+// *************** nav menu responsive ***************//
+
+// launch Nav event
+navIcon.forEach((btn) => btn.addEventListener("click", editNav)) ;
+
+// close Nav event
+navBtnClos.forEach((btn)  =>btn.addEventListener("click", stopNav)) ;
+
+function editNav() {
+  if (changNav.className === "topnav") { // verification class name 
+    launcNav();
+  } else {
+    stopNav()
+  }
+}
+
+function launcNav() { 
+  navBar.style.display = "flex";
+  changNav.className += " responsive"; // add class name  ".topnav.responsive"
+}
+
+function stopNav() {
+  navBar.style.display = "none";
+  changNav.className = "topnav";
+}
 
