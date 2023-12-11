1c1cc91bd1491d1dc198f9a4388511cf29ea1c4e
Mohammed Habib Hamza <microrepair33@gmail.com>
2023-12-11 10:34:04 +0100

    all function submit verify

--- starterOnly/js/modal.js
+++ starterOnly/js/modal.js
@@ -1,36 +1,12 @@
 
 // DOM Elements
 
-// Modal declaration elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
-const modalIcon = document.querySelector(".close-modal");
-
 // Nav declaration elements
-let changNav = document.querySelector("#myTopnav");
+const changNav = document.querySelector("#myTopnav");
 const navBar = document.querySelector(".main-navbar");
 const navBtnClos = document.querySelectorAll(".close-nav");
-const navIcon = document.querySelectorAll(".icon")
-
-// *************** modal event ***************//
-
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
-// Stop modal event
-modalIcon.addEventListener("click", stopModal);
-
-// launch modal form
-function launchModal() {
-  modalbg.style.display = "block";
-}
-
-// close  modal form
-function stopModal() {
-  modalbg.style.display = "none";
-}
-
+const navIcon = document.querySelectorAll(".icon");
+const bgNav = document.querySelector(".bground-nav")
 // *************** nav menu responsive  event ***************//
 
 // launch Nav event
@@ -38,7 +14,6 @@ navIcon.forEach((btn) => btn.addEventListener("click", editNav)) ;
 
 // close Nav event
 navBtnClos.forEach((btn)  =>btn.addEventListener("click", stopNav)) ;
-
 function editNav() {
   if (changNav.className === "topnav") { // verification class name 
     launcNav();
@@ -51,184 +26,154 @@ function launcNav() {
   navBar.style.display = "flex";
   changNav.className += " responsive"; // add class name  ".topnav.responsive"
 }
-
 function stopNav() {
   navBar.style.display = "none";
   changNav.className = "topnav";
 }
 
+// Modal declaration elements
+const modalbg = document.querySelector(".bground");
+const modalBtn = document.querySelectorAll(".modal-btn");
+const formData = document.querySelectorAll(".formData");
+const modalIcon = document.querySelector(".close-modal");
+let modalContent = document.querySelector(".content");
+let modalBody = document.querySelector(".modal-body");
+let dateRe =/^\d{4}[\/\-](0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])$/;
 
-// *******verification a valid input in form  modal *****//
-// **** form declaration elements ***//
-let formModal = document.querySelector ("[name ='reserve']");
-let btnValidate = document.querySelector('.btn-submit');
-let firstNameValid = false;
-let lastNameValid = false ;
-let emailValid = false ;
-let birthDateValid = false ;
+// *************** modal event ***************//
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
-formModal.addEventListener('submit' , function(e){
-  if(firstNameValid === false || lastNameValid === false || emailValid === false || birthDateValid === false ){
-        e.preventDefault(); // ignor a default validate submit
-        checkFirstName();
-        checkLastName();
-        checkEmail();
-        checkBirthDate();
-      }
-      else{
-        console.log('ok')
-      }
+// close modal event
+modalIcon.addEventListener("click", stopModal);
+
+// function launch modal form
+function launchModal() {
+  modalbg.style.display = "block";
+  modalBody.style.display ="block";
+}
+
+// function close  modal form
+function stopModal() {
+  modalbg.style.display = "none";
+  displayConfirm.remove();
+}
+
+// *************** submit form ***************//
+let emailRe =/^[a-z0-9._-]+@[a-z0-9]{2,}\.[a-z]{2,6}$/;
+let formValid = false;
+document.querySelector("form").addEventListener('submit' , (e) => {
+ e.preventDefault(); // ignor a default validate submit
+  if(formValid === false){
+    checkInput()
+  } else {
+    console.log("go");
+    let displayConfirm = modalBody.cloneNode();
+    modalBody.after(displayConfirm);
+    displayConfirm.style.display = "flex";
+    modalBody.style.display = "none"; 
+    displayConfirm.classList.add('display-confirm') // add a new class 
     
+    // creation a new elements  in body
+    let btnConfirm = document.createElement("button");
+    let textBtn = document.createTextNode("Fermer")
+    let textConfirm = document.createTextNode("Merci pour votre inscription");
+    btnConfirm.appendChild(textBtn);
+    displayConfirm.appendChild(textConfirm);
+    displayConfirm.appendChild(btnConfirm);
+    btnConfirm.classList.add('btn-signup');
+    modalIcon.classList.add('iconConfirmClose');
+    modalIcon.addEventListener('click',function(){
+    displayConfirm.remove();
+      stopModal();
+    });
+    btnConfirm.addEventListener('click',function(){
+      displayConfirm.remove();
+      stopModal();
+    });
+  }    
 })
-// function display style errors messages  
-const setError = (element, message) => {
-  const inputControl = element.parentElement;
-  const errorDisplay = inputControl.querySelector('.error');
 
-  errorDisplay.innerText = message;
-  inputControl.classList.add('error');
-  inputControl.classList.remove('success')
+
+// ******* function check errors  *****//
+let checkInput = () => {
+  let allInput = document.querySelectorAll ('input');
+  // let radioChecked = false;
+  let radioCheckedNumber = 0
+  allInput.forEach (input => {
+    if(input.name === 'first' || input.name === 'last'){ // conditions validate  first and last name 
+        if(input.value != "" && input.value.length >= 2 ){
+          result (input);
+        } else {
+          result (input,"Veuillez entrer 2 caractères ou plus ");
+        }
+    } 
+    if (input.name === 'email') { // conditions validate  email 
+        if(input.value != "" && input.value.match (emailRe)){
+          result (input);
+        } else {
+          result (input,"Veuillez entrer un email valide");
+        }
+    } 
+    if (input.name === 'birthdate'){ // conditions validate  birthdate 
+        if(input.value != "" && input.value.match (dateRe)){
+          result (input);
+        } else {
+          result (input ,"Veuillez entrer une date de naissance valide")
+        }
+    }
+    if (input.name === 'quantity'){ // conditions validate  quantity games   
+      if(input.value < 100 ){
+          result (input);
+      } else {
+          result (input,"Le maximum de participation est de  99");
+      }
+    }
+    if (input.name === 'location'){ // conditions validate  location city    
+            console.log('ok ',input.value,' ', input.checked);
+          if (input.checked) { // verify if is realy checked 
+            radioCheckedNumber = radioCheckedNumber + 1
+          }
+          else{
+            radioCheckedNumber = radioCheckedNumber + 0;
+          }
+          if(radioCheckedNumber !== 0){
+              // radioChecked = true
+              // formValid = true
+              result (input)
+          }
+          else {
+              // radioChecked = false
+              // formValid = false
+              result (input, "Veuillez choisir  une ville")
+          }
+          // console.log('result checkbox ',radioChecked);
+    }
+    if (input.id === 'checkbox1'){ // conditions validate  conditions use   
+      if(input.checked ){
+          result (input);
+      } else {
+          result (input,"Vous devez vérifier que vous acceptez les termes et conditions");
+      }
+    }
+
+  })
 }
 
-// function display style success messages  
-const setSuccess = element => {
+let result = (element, messageError ) => { // function result message 
   const inputControl = element.parentElement;
-  const errorDisplay = inputControl.querySelector('.error');
-
-  errorDisplay.innerText = '';
-  inputControl.classList.add('success');
-  inputControl.classList.remove('error');
+  const errorDisplay = inputControl.querySelector ('.error');
+  errorDisplay.innerText = messageError || '';
+  if(messageError){
+     inputControl.classList.add ('error');
+    inputControl.classList.remove ('success');
+    formValid = false ;
+  }
+  else{
+    inputControl.classList.add ('success');
+    inputControl.classList.remove ('error');
+    formValid = true ;
+  }  
 };
+  
 
-// function first  name errors prevent and messages
-
-let checkFirstName = function(){
-  let firstName = document.querySelector("[name = 'first']");
-  if(firstName.value.trim() != "" && firstName.value.length >= 2 ){
-     firstNameValid === true;
-     setSuccess (firstName)
-  }
-  else{
-    setError(firstName,"Veuillez entrer 2 caractères ou plus pour le champ du du prenom")
-     firstNameValid === false;
-  }
-}
-// function last name errors prevent and messages
-let checkLastName = function(){
-  let lastName = document.querySelector("[name = 'last']");
-  if(lastName.value.trim() != "" && lastName.value.length >= 2 ){
-     lastNameValid === true;
-     setSuccess (lastName);
-  }
-  else{
-     lastNameValid === false;
-     setError(lastName,"Veuillez entrer 2 caractères ou plus pour le champ du nom");
-  }
-}
-// function email errors prevent and messages
-const checkEmail = function(){
-    let email = document.querySelector("[name = 'email']");
-    let  emailRe =/^[a-z0-9._-]+@[a-z0-9]{2,}\.[a-z]{2,6}$/;  
-    if(email.value != "" && email.value.match(emailRe)){
-      emailValid === true;
-      setSuccess (email);
-    }
-    else{
-       emailValid === false;
-       setError(email,"Veuillez entrer un email valide");
-    }
-  }
-
-// function birth date  errors prevent and messages
-  let checkBirthDate = function(){
-    let  age = document.querySelector("[name = 'birthdate']");
-    let  dateRe =/^\d{4}[\/\-](0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])$/;
-    if(age.value != "" && age.value.match(dateRe)){
-       birthDateValid === true;
-       setSuccess (age);
-    }
-    else{
-      birthDateValid === false;
-      setError(age,"Veuillez entrer une date de naissance  valide");
-    }
-  }
-
-  // function first  name errors prevent and messages
-
-
-
-
-
-  // const form = document.getElementById('form');
-  // const username = document.getElementById('username');
-  // const email = document.getElementById('email');
-  // const password = document.getElementById('password');
-  // const password2 = document.getElementById('password2');
-  
-  // form.addEventListener('submit', e => {
-  //     e.preventDefault();
-  
-  //     validateInputs();
-  // });
-  
-  // const setError = (element, message) => {
-  //     const inputControl = element.parentElement;
-  //     const errorDisplay = inputControl.querySelector('.error');
-  
-  //     errorDisplay.innerText = message;
-  //     inputControl.classList.add('error');
-  //     inputControl.classList.remove('success')
-  // }
-  
-  // const setSuccess = element => {
-  //     const inputControl = element.parentElement;
-  //     const errorDisplay = inputControl.querySelector('.error');
-  
-  //     errorDisplay.innerText = '';
-  //     inputControl.classList.add('success');
-  //     inputControl.classList.remove('error');
-  // };
-  
-  // const isValidEmail = email => {
-  //     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  //     return re.test(String(email).toLowerCase());
-  // }
-  
-  // const validateInputs = () => {
-  //     const usernameValue = username.value.trim();
-  //     const emailValue = email.value.trim();
-  //     const passwordValue = password.value.trim();
-  //     const password2Value = password2.value.trim();
-  
-  //     if(usernameValue === '') {
-  //         setError(username, 'Username is required');
-  //     } else {
-  //         setSuccess(username);
-  //     }
-  
-  //     if(emailValue === '') {
-  //         setError(email, 'Email is required');
-  //     } else if (!isValidEmail(emailValue)) {
-  //         setError(email, 'Provide a valid email address');
-  //     } else {
-  //         setSuccess(email);
-  //     }
-  
-  //     if(passwordValue === '') {
-  //         setError(password, 'Password is required');
-  //     } else if (passwordValue.length < 8 ) {
-  //         setError(password, 'Password must be at least 8 character.')
-  //     } else {
-  //         setSuccess(password);
-  //     }
-  
-  //     if(password2Value === '') {
-  //         setError(password2, 'Please confirm your password');
-  //     } else if (password2Value !== passwordValue) {
-  //         setError(password2, "Passwords doesn't match");
-  //     } else {
-  //         setSuccess(password2);
-  //     }
-  
-  // };
-  
